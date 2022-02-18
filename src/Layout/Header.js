d7ac2a2fd48a89ac52de32df6bf34f8c642a1f3d
andurebel <andu.trica@gmail.com>
2022-02-18 20:40:09 +0200

    added initial files

---
+++ src/Layout/Header.js
@@ -0,0 +1,13 @@
+export const Header = (props) => {
+  return (
+    <>
+      <header>
+        <h1>My meals</h1>
+        <button>Cart</button>
+      </header>
+      <div></div>
+    </>
+  );
+};
+
+export default Header;
