e994e0741c7bc5b3875d210d587c70bdf8ce8369
andurebel <andu.trica@gmail.com>
2022-02-19 21:38:33 +0200

    basic Meals components done

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,51 @@
+import React from "react";
+import classes from "./AvailableMeals.module.css";
+
+const DUMMY_MEALS = [
+  {
+    id: 1,
+    name: "Soup",
+    description:
+      "Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.",
+    price: 99,
+  },
+  {
+    id: 2,
+    name: "Grilled Burger",
+    description:
+      "Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.",
+    price: 49,
+  },
+  {
+    id: 3,
+    name: "Pizza",
+    description:
+      "Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui.\n\nProin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.",
+    price: 1,
+  },
+  {
+    id: 4,
+    name: "Italian Pasta",
+    description:
+      "In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.",
+    price: 10,
+  },
+  {
+    id: 5,
+    name: "Steak",
+    description:
+      "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.\n\nCras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
+    price: 7,
+  },
+];
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  return (
+    <section className={classes.meals}>
+      <ul>{mealsList}</ul>
+    </section>
+  );
+};
+
+export default AvailableMeals;
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,22 @@
+import React from "react";
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h3>Very good food for you</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia
+        eligendi nostrum voluptatem quidem vel maiores deserunt facere ex,
+        libero pariatur repellendus assumenda asperiores! Voluptatibus,
+        consequuntur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+        consectetur placeat laboriosam quaerat repellendus dignissimos!
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
