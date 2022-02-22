239cf8ab995ecf82b4fd1d2887cc1c0f4e45a81f
andurebel <andu.trica@gmail.com>
2022-02-22 22:53:20 +0200

    cartItem separated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 export const Cart = (props) => {
   const cartCtx = useContext(CartContext);
@@ -9,10 +10,21 @@ export const Cart = (props) => {
   const totalAmount = `$ ${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddItem = (item) => {};
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li key={item.name}>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddItem.bind(null, item)}
+        />
       ))}
     </ul>
   );
